252b9dce1666d9cac922d00701e5501434545756
Aisling Dwyer <x21123748@student.ncirl.ie>
2022-08-02 15:12:06 +0100

    day 18 debugged

--- day18/script.js
+++ day18/script.js
@@ -8,7 +8,7 @@ let activeSlide = 0
 rightBtn.addEventListener('click', () => {
     activeSlide++
 
-    if(activeSlide>slides.length-1) {
+    if(activeSlide > slides.length - 1) {
         activeSlide = 0
     }
 
@@ -17,10 +17,10 @@ rightBtn.addEventListener('click', () => {
     setActiveSlide()
 })
 
-leftBtn.addEventListener((click), () => {
-    activeSlide++
+leftBtn.addEventListener('click', () => {
+    activeSlide--
 
-    if(activeSlide<0) {
+    if(activeSlide < 0) {
        activeSlide = slides.length -1
     }
 
@@ -28,6 +28,8 @@ leftBtn.addEventListener((click), () => {
 
     setActiveSlide()
 })
+
+setBgToBody()
 
 
 function setBgToBody() {
